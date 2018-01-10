426371b8f227f9a6c954dcb96dfe70fe6e778e77
alex <qwert2603@mail.ru>
2018-01-10 11:26:22 +0700

    \

--- library/src/main/java/com/qwert2603/andrlib/demo/DemoItem.kt
+++
@@ -1,8 +0,0 @@
-package com.qwert2603.andrlib.demo
-
-import com.qwert2603.andrlib.model.IdentifiableLong
-
-data class DemoItem(
-        override val id: Long,
-        val i: Int
-) : IdentifiableLong
--- library/src/main/java/com/qwert2603/andrlib/demo/DemoView.kt
+++
@@ -1,5 +0,0 @@
-package com.qwert2603.andrlib.demo
-
-import com.qwert2603.andrlib.base.mvi.load_refresh.list.ListView
-
-interface DemoView : ListView<DemoViewState>
